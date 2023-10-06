d5ad97311cb2dd1a70172dd837a93bf8222198e0
Isra <Isra@DESKTOP-64KKLPF>
2023-10-05 22:24:39 -0600

    Agregar archivos de proyecto.

---
+++ Practica5U2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Ingresa el número mínimo del rango: ");
+int min, max;
+min = int.Parse(Console.ReadLine());
+Console.Write("Ingresa el número máximo del rango: ");
+max = int.Parse(Console.ReadLine());
+if (min >= max)
+{
+    Console.WriteLine("El número mínimo debe ser menor que el número máximo.");
+}
+else
+{
+    Random aleatorio = new Random();
+    int numeroGenerado = aleatorio.Next(min, max + 1);
+    Console.WriteLine($"Número aleatorio generado entre {min} y {max}: {numeroGenerado}");
+}
+Console.ReadKey();
